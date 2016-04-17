0faca39fbdad146336771623c3ecd938019de9ec
Nypro <steveakinyemi@gmail.com>
2016-04-17 16:29:25 +0100

    initial commit

---
+++ OpenGLTutorial/main.cpp
@@ -0,0 +1,60 @@
+//Jeff Chastine
+#include <Windows.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <SOIL.h>
+#include "glm/vec2.hpp"
+#include <iostream>
+
+using namespace std;
+
+void changeViewPort(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+void render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+	// Initialize GLUT
+	glutInit(&argc, argv);
+	// Set up some memory buffers for our display
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	// Set the window size
+	glutInitWindowSize(800, 600);
+	// Create the window with the title "Hello,GL"
+	glutCreateWindow("Hello, GL");
+	// Bind the two functions (above) to respond when necessary
+	glutReshapeFunc(changeViewPort);
+	glutDisplayFunc(render);
+
+	// Very important!  This initializes the entry points in the OpenGL driver so we can 
+	// call all the functions in the API.
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
+	}
+
+	/****************************/
+
+	GLuint tex_2d;
+	tex_2d = SOIL_load_OGL_texture
+		(
+		"C:/Users/Nypro/Desktop/GUI/free-flat-design-kit-25.png",
+		SOIL_LOAD_AUTO,
+		SOIL_CREATE_NEW_ID,
+		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+		);
+	/*****************************/
+
+	glutMainLoop();
+	return 0;
+}
